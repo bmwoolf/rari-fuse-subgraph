dd7262b6b0563a7630f2f2fcd6202955757b58a1
Bradley Woolf <bradleymwoolf@gmail.com>
2021-11-22 11:02:39 -0800

    comment out logs and add the if/else conditions for testing in handleBorrow

--- src/mappings/cToken.ts
+++ src/mappings/cToken.ts
@@ -73,10 +73,8 @@ export function handleNewComptroller(event: NewComptroller): void {
 }
 
 export function handleBorrow(event: Borrow): void {
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 45, begin handleBorrow()`,
-    []
-  );
+  log.warning(`🚨cToken line 45, begin handleBorrow()`, []);
+  // create a skeleton for the CToken that someone wants to borrow
   const entity = CtokenSchema.load(event.address.toHexString());
   //entity.totalBorrows = event.params.totalBorrows;
 
@@ -101,14 +99,25 @@ export function handleBorrow(event: Borrow): void {
   );
 
   let balanceCall = erc20.try_balanceOf(event.params.borrower);
-  log.warning("handleBorrow: trying erc20Balance: token: {} - borrower: {}", [
-    erc20._address.toHexString(),
-    event.params.borrower.toHexString(),
-  ]);
+  // log.warning(
+  //   "🚨 handleBorrow: trying erc20Balance: token: {} - borrower: {}",
+  //   [erc20._address.toHexString(), event.params.borrower.toHexString()]
+  // );
+
   // Todo - Fix error on borrow in this codeblock
   if (!balanceCall.reverted) {
-    // borrowFromMarket(event,account, market,event.params.borrowAmount, [], [], erc20.balanceOf(event.params.borrower), [], []);
-    // updateCtoken(event, entity, event.address);
+    borrowFromMarket(
+      event,
+      account,
+      market,
+      event.params.borrowAmount,
+      [],
+      [],
+      erc20.balanceOf(event.params.borrower),
+      [],
+      []
+    );
+    updateCtoken(event, entity, event.address);
   } else {
     log.warning(
       "handleBorrow: erc20 balanceCall Reverted: token: {} - borrower: {}",
@@ -118,17 +127,14 @@ export function handleBorrow(event: Borrow): void {
 
   entity.save();
 
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 122, end handleBorrow()`,
-    []
-  );
+  // log.warning(
+  //   `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 122, end handleBorrow()`,
+  //   []
+  // );
 }
 
 export function handleRepayBorrow(event: RepayBorrow): void {
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 128, begin handleRepayBorrow()`,
-    []
-  );
+  // log.warning(`🚨 cToken line 128, begin handleRepayBorrow()`, []);
   const entity = CtokenSchema.load(event.address.toHexString());
   //entity.totalBorrows = event.params.totalBorrows;
 
@@ -165,51 +171,36 @@ export function handleRepayBorrow(event: RepayBorrow): void {
 
   updateCtoken(event, entity, event.address);
   entity.save();
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 168, end handleRepayBorrow()`,
-    []
-  );
+  // log.warning(`🚨 cToken line 168, end handleRepayBorrow()`, []);
 }
 
 export function handleMint(event: Mint): void {
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 176, begin handleMint()`,
-    []
-  );
+  // log.warning(`🚨 cToken line 176, begin handleMint()`, []);
   const entity = CtokenSchema.load(event.address.toHexString());
 
   updateCtoken(event, entity, event.address);
   entity.save();
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 184, end handleMint()`,
-    []
-  );
+  // log.warning(`🚨 cToken line 184, end handleMint()`, []);
 }
 
 export function handleLiquidateBorrow(event: LiquidateBorrow): void {
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 191, begin handleLiquidateBorrow()`,
-    []
-  );
+  // log.warning(`🚨 cToken line 191, begin handleLiquidateBorrow()`, []);
   const entity = CtokenSchema.load(event.address.toHexString());
 
   updateCtoken(event, entity, event.address);
   entity.totalSeizedTokens = entity.totalSeizedTokens.plus(
     event.params.seizeTokens
   );
-  log.warning(`🚨2 update totalSeized to {}`, [
-    event.params.seizeTokens.toString(),
-  ]);
+  // log.warning(`🚨2 update totalSeized to {}`, [
+  //   event.params.seizeTokens.toString(),
+  // ]);
   const pool = ComptrollerSchema.load(entity.pool);
   pool.totalSeizedTokens = pool.totalSeizedTokens.plus(
     event.params.seizeTokens
   );
   pool.save();
   entity.save();
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 210, end handleLiquidateBorrow()`,
-    []
-  );
+  // log.warning(`🚨 cToken line 210, end handleLiquidateBorrow()`, []);
 }
 
 export function handleAccrueInterest(event: AccrueInterest): void {
@@ -224,33 +215,27 @@ function updateCtoken(
   entity: CtokenSchema | null,
   address: Address
 ): void {
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 227, begin updateCtoken()`,
-    []
-  );
-  log.warning("🚨🚨🚨 cToken line 231 Updating Ctoken  now {}", [
-    address.toHexString(),
-  ]);
-
   updateETHPrice();
+
+  // log.warning();
 
   let util = Utility.load("0");
   const ethUSD = util.ethPriceInDai;
 
-  log.debug("🚨🚨🚨 cToken line 240 updateCtoken: ethUSDPrice: {}", [
-    ethUSD.toString(),
-  ]);
+  // log.debug("🚨🚨🚨 cToken line 240 updateCtoken: ethUSDPrice: {}", [
+  //   ethUSD.toString(),
+  // ]);
 
   //update price from oracle on pool
   const pool = ComptrollerSchema.load(entity.pool);
   const oracle = PriceOracle.bind(pool.priceOracle as Address);
   const asset = UnderlyingAssetSchema.load(entity.underlying);
 
-  log.debug("updateCtoken: asset: {}", [
-    pool.address.toHexString(),
-    oracle._address.toHexString(),
-    asset.address.toHexString(),
-  ]);
+  // log.debug("updateCtoken: asset: {}", [
+  //   pool.address.toHexString(),
+  //   oracle._address.toHexString(),
+  //   asset.address.toHexString(),
+  // ]);
 
   const _price = oracle.try_getUnderlyingPrice(address);
   if (!_price.reverted) {
@@ -260,10 +245,10 @@ function updateCtoken(
         entity.underlyingToken = asset.address;
         entity.underlyingPrice = asset.price; */
 
-  log.debug("🚨🚨🚨 updateCtoken line 263: 3 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨 updateCtoken line 263: 3 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   const instance = CToken.bind(address);
   const erc20 = ERC20.bind(Address.fromString(entity.underlying));
@@ -272,10 +257,10 @@ function updateCtoken(
     Address.fromString(entity.underlying)
   );
 
-  log.debug("🚨🚨🚨 updateCtoken line 275: 4 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨🚨🚨 updateCtoken line 275: 4 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   entity.name = instance.name();
   entity.symbol = instance.symbol();
@@ -285,17 +270,17 @@ function updateCtoken(
     entity.underlyingBalance = _balance.value;
   }
 
-  log.debug("🚨🚨🚨 updateCtoken line 288: 5 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨 updateCtoken line 288: 5 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   const totalSupply = calculateCTokenTotalSupply(instance);
 
-  log.debug("🚨🚨🚨 updateCtoken line 292: 6 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨 updateCtoken line 292: 6 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   //this one is seperate from the other if block because usd increase doesn't always mean that real amount increased
   if (entity.totalSupply.ge(_price.value)) {
@@ -311,10 +296,10 @@ function updateCtoken(
     }
   }
 
-  log.debug("🚨🚨🚨 updateCtoken line 314: 7 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨 updateCtoken line 314: 7 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   entity.totalSupply = totalSupply;
   const newTotalSupplyUSD = getTotalInUSD(totalSupply, ethUSD, asset.price);
@@ -343,10 +328,10 @@ function updateCtoken(
   }
   entity.totalSupplyUSD = newTotalSupplyUSD;
 
-  log.debug("🚨🚨🚨 updateCtoken line 346: 8 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨 updateCtoken line 346: 8 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   const _cash = instance.try_getCash();
   if (!_cash.reverted) {
@@ -395,12 +380,14 @@ function updateCtoken(
     entity.liquidityUSD = newLiquidityUSD;
   }
 
-  log.debug("🚨🚨🚨 updateCtoken line 398: 9 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨🚨🚨 updateCtoken line 398: 9 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
+  // i think this is where the error is
   const _borrowRatePerBlock = instance.try_borrowRatePerBlock();
+
   if (!_borrowRatePerBlock.reverted) {
     entity.borrowRatePerBlock = _borrowRatePerBlock.value;
     entity.borrowAPR = convertMantissaToAPR(
@@ -408,10 +395,10 @@ function updateCtoken(
     );
   }
 
-  log.debug("🚨🚨🚨 updateCtoken line 411: 10 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨 updateCtoken line 411: 10 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   const _totalBorrow = instance.try_totalBorrowsCurrent();
   if (!_totalBorrow.reverted) {
@@ -458,10 +445,10 @@ function updateCtoken(
     entity.totalBorrowUSD = newBorrowUSD;
   }
 
-  log.debug("🚨🚨🚨 updateCtoken line 461: 11 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨 updateCtoken line 461: 11 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   const _totalReserves = instance.try_totalReserves();
   if (!_totalReserves.reverted) {
@@ -491,31 +478,25 @@ function updateCtoken(
   const _supplyRatePerBlock = instance.try_supplyRatePerBlock();
   if (!_supplyRatePerBlock.reverted) {
     entity.supplyRatePerBlock = _supplyRatePerBlock.value;
-    log.warning(
-      "🚨🚨🚨🚨 updateCtoken line 494 updating supplyAPY to {} from {}",
-      [
-        convertMantissaToAPY(
-          BigDecimal.fromString(_supplyRatePerBlock.value.toString())
-        ).toString(),
-        _supplyRatePerBlock.value.toString(),
-      ]
-    );
+    log.warning("🚨 updateCtoken line 494 updating supplyAPY to {} from {}", [
+      convertMantissaToAPY(
+        BigDecimal.fromString(_supplyRatePerBlock.value.toString())
+      ).toString(),
+      _supplyRatePerBlock.value.toString(),
+    ]);
     entity.supplyAPY = convertMantissaToAPY(
       BigDecimal.fromString(_supplyRatePerBlock.value.toString())
     );
   }
 
-  log.debug("🚨🚨🚨🚨 updateCtoken line 508: 12 - {} - {}", [
-    entity.id,
-    entity.underlying,
-  ]);
+  // log.debug("🚨 updateCtoken line 508: 12 - {} - {}", [
+  //   entity.id,
+  //   entity.underlying,
+  // ]);
 
   entity.save();
   pool.save();
   asset.save();
 
-  log.warning(
-    `🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 cToken line 515, end updateCtoken()`,
-    []
-  );
+  log.warning(`🚨 cToken line 515, end updateCtoken()`, []);
 }
